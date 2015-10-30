021ca3c8957c2655964e7f96970e1026d8b842d0
Igor Zygin <igorzygin@Igors-iMac.local>
2015-10-30 17:51:19 +0200

    blabla

--- tracking.js
+++ tracking.js
@@ -1,8 +1,31 @@
 var path = require('path');
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://postgres@px.adbox.kz:5445/postgres';
-var client = new pg.Client(connectionString);
-client.connect();
+var client
+
+
+function get_client() {
+ if (client == undefined) {
+  client = new pg.Client(connectionString);
+  client.connect();
+ }
+ return client
+}
+
+function executeSafe(query) {
+ try {
+   get_client().query(query,function (error, result)
+   {
+      if (error)
+      {
+          console.log(error)
+      }
+   })
+ } catch (err) {
+   console.log(err)
+ }
+}
+
 
 var Cookies = require( "cookies" )
 var parser = require('ua-parser-js');
@@ -12,10 +35,10 @@ var js_template
 String.prototype.format = function() {
     var formatted = this;
     for (var i = 0; i < arguments.length; i++) {
-        var regexp = new RegExp('\\{'+i+'\\}', 'gi');  
+        var regexp = new RegExp('\\{'+i+'\\}', 'gi');
         if (arguments[i]==undefined) {
           arguments[i] = "NULL"
-        }      
+        }
         formatted = formatted.replace(regexp, arguments[i]);
     }
     return formatted;
@@ -41,7 +64,7 @@ function get_cookie(req, res) {
       local_id=get_random_string(20)
       res.end("advertCookieCallback_AdvertNetwork('{0}')".format(local_id))
       console.log('NEW LOCAL_ID',local_id)
-      // async(function(){  log_local_id(site_id,local_id)   })
+       async(function(){log_local_id(site_id,local_id)   })
     }
 }
 
@@ -52,9 +75,16 @@ function get_iframe(req, res) {
 
 
    page_id = req.params.id
-   local_id=req.query.local_id
-   session_id=req.query.session_id
-   ref_url=req.query.ref_url
+   str = req.query.xtra
+   query = {}
+   if (str!=undefined)
+   {
+    str = new Buffer(str, 'base64').toString('utf8')
+    console.log(str)
+    query = JSON.parse(str)
+   }
+   local_id=query['local_id']
+   session_id=query['session_id']
 
    global_id=cookies.get("userID_AdvertNetwork")
 
@@ -66,8 +96,8 @@ function get_iframe(req, res) {
    }
    res.end("")
 
+   async(function() {log_session_id(session_id,page_id,req, query)})
    async(function() {logPage(session_id,local_id,global_id, page_id)})
-   async(function() {log_session_id(session_id,page_id, req)})
    async(function() {set_user_id_to_session(session_id,local_id)})
  }
 }
@@ -82,11 +112,11 @@ function session_time(req, res) {
   async(function(){update_session_time(session_id,Date.now(),time_active)})
 }
 
-function get_start_js(req, res) {	
+function get_start_js(req, res) {
 	session_id = get_random_string(20)
 	page_id = req.params.id
 	res.end(js_template.format(session_id, page_id))
-	async(function(){log_session_id(session_id, page_id, req)})
+//	async(function(){log_session_id(session_id, page_id, req)})
 }
 
 
@@ -96,12 +126,10 @@ module.exports.session_time = session_time;
 module.exports.get_start_js = get_start_js;
 
 
-function log_session_id(session_id,page_id, req) {
+function log_session_id(session_id,page_id, req, query) {
   ip_adr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;//req.connection.remoteAddress
   console.log(req.headers['x-forwarded-for'])
   console.log(req.connection.remoteAddress)
-  // agent = req.headers['user-agent']
-  // id=req.params.id
 
   var ua = parser(req.headers['user-agent']);
   browser=ua['browser']['name']
@@ -120,17 +148,8 @@ function log_session_id(session_id,page_id, req) {
    city = ""
   }
 
-  str = req.query.xtra
-  if (xtra!=undefined)
-  {
-   str = new Buffer(str, 'base64').toString('utf8')
-   query = JSON.parse(str)
-  } else {
-   xtra = {}
-  }
-
-
-  url = query['url']
+  url = query['site_URL']
+  extra = query['extra']
 
 
   query_str = ("INSERT INTO page_sessions_link "+
@@ -138,34 +157,34 @@ function log_session_id(session_id,page_id, req) {
   "SELECT \'{0}\',\'{1}\',\'{2}\',\'{3}\',\'{4}\',\'{5}\',\'{6}\',\'{7}\', \'{8}\', \'{9}\' "+
   "WHERE NOT EXISTS (SELECT id FROM page_sessions_link WHERE session_id=\'{1}\')").format(page_id,session_id,os,os_version,browser,browser_version,country,city, ip_adr, url)
   console.log(query_str)
-  query = client.query(query_str);   
+  executeSafe(query_str);
 }
 
-function update_session_time(session_id, time) {  
+function update_session_time(session_id, time) {
   query_str = "UPDATE page_sessions_link SET session_updated=now(), active_time={1} WHERE session_id=\'{0}\'".format(session_id,time_active)
   console.log(query_str)
-  query = client.query(query_str);   
+  executeSafe(query_str);
 }
 
 
 function logPage(session_id, local_id, global_id, page_id) {
 	console.log(session_id, local_id, global_id, page_id)
 	query_str = "INSERT INTO localid_site_link (site_id, local_user_id, global_user_id) VALUES ((select site_id from rotator_userpixel where unique_code=\'{0}\' limit 1),\'{1}\',\'{2}\')".format(page_id,local_id,global_id)
-  console.log(query_str)
-  query = client.query(query_str);
+ console.log(query_str)
+ executeSafe(query_str);
 }
 
 function set_user_id_to_session(session_id,local_id) {
   console.log(session_id, local_id, global_id, page_id)
   query_str = "UPDATE page_sessions_link SET local_user_id=\'{0}\' WHERE session_id=\'{1}\'".format(local_id,session_id)
   console.log(query_str)
-  query = client.query(query_str);
+  executeSafe(query_str);
 }
 
 function log_local_id(site_id,local_id) {
   query_str = "INSERT INTO localid_site_link (site_id, local_user_id) VALUES ((select site_id from rotator_userpixel where unique_code=\'{0}\' limit 1),\'{1}\')".format(page_id,local_id)
   console.log(query_str)
-  query = client.query(query_str);
+  executeSafe(query_str);
 }
 
 
